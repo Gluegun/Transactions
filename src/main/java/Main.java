2741573b4bbcead5931286eebe11a180991212c2
Gluegun <kirill.yudintsev@gmail.com>
2020-04-04 14:02:07 +0300

    changed tests are made in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,39 @@ public class Main {
 
     private static Logger logger = LogManager.getRootLogger();
     private static final Marker INPUT_OK = MarkerManager.getMarker("INPUT_OK");
+    private static Bank bank;
+    private static List<Account> accounts;
 
     public static void main(String[] args) {
 
-        Bank bank = new Bank();
+        bank = new Bank();
 
         int amountOfAccounts = 30;
         logger.info(INPUT_OK, "Создано " + amountOfAccounts + " счетов");
 
-        List<Account> accounts = createAccountList(amountOfAccounts); // создаем список счетов со случайным количеством ДС на счету
+        accounts = createAccountList(amountOfAccounts); // создаем список счетов со случайным количеством ДС на счету
 
         addAccountsToBank(bank, accounts); // добавляем счета в банк
 
         System.out.println("Before operations");
         bank.printBankMoneyAmount(); // сумма всех ДС на всех счетах в банке
 
-        startThreadsWithTransferMethod(bank, accounts, 10, 3000); // метод, запускающий потоки, выполняющие метод трансфер класса Банк
+        long start = System.currentTimeMillis();
+
+        List<Thread> threads = createThreadsList(15, 10000, 49000);
+
+        threads.forEach(Thread::start);
+
+        for(Thread thread : threads) {
+            try {
+                thread.join();
+            }
+            catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        System.out.printf("Время работы программы: %d мс\n", (System.currentTimeMillis() - start));
 
         System.out.println("After operations:");
         bank.printBankMoneyAmount(); // проверка, что после всех операций сумма не изменилась
@@ -34,27 +51,24 @@ public class Main {
 
     }
 
-    private static void startThreadsWithTransferMethod(Bank bank, List<Account> accounts, int amountOfThreads, int amountOfTransactions) {
+    private static List<Thread> createThreadsList(int amountOfThreads, int amountOfTransactions, int rangeOfTransactionAmount) {
+
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < amountOfThreads; i++) {
-            Thread thread = new Thread(() -> {
+            threads.add(new Thread(() -> {
                 for (int j = 0; j < amountOfTransactions; j++) {
                     String from = accounts.get((int) (Math.random() * accounts.size())).getAccNumber();
                     String to = accounts.get((int) (Math.random() * accounts.size())).getAccNumber();
-                    int sum = (int) (Math.random() * 52000);
+                    int sum = (int) (Math.random() * rangeOfTransactionAmount);
                     bank.transfer(from, to, sum);
                 }
-            });
-            thread.start();
-            try {
-                thread.join();
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
+            }));
         }
+        return threads;
     }
 
-    public static List<Account> createAccountList(int amount) {
+    private static List<Account> createAccountList(int amount) {
         List<Account> accounts = new ArrayList<>();
         for (int i = 0; i < amount; i++) {
             accounts.add(new Account((long) (1 + Math.random() * 1000000)));
@@ -62,7 +76,7 @@ public class Main {
         return accounts;
     }
 
-    public static void addAccountsToBank(Bank bank, List<Account> accounts) {
+    private static void addAccountsToBank(Bank bank, List<Account> accounts) {
         String accNumber;
 
         for (Account account : accounts) {
